1e98c034e6cc766b0b62ec2a04d4324fcbd2255a
sanjana888 <155309673+sanjana888@users.noreply.github.com>
2024-12-07 21:43:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World from Java!");
+        System.out.println("Hello, Worllld from Java!");
     }
 }
